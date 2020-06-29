24244c88d99298ce93d38bdaf85fe4bbb7dae4d3
gyata_shoa <felixasamoah539@gmail.com>
2020-06-29 11:10:54 +0000

    no definitions error fixed and fonts added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -23,22 +23,63 @@ class _HomePageState extends State<HomePage> {
     _stream = _streamController.stream;
   }
 
+  bool _requesting = false;
+  Future<Response> _onTimeout(context) {
+    // Scaffold.of(context).showSnackBar(SnackBar(
+    //   content: Text("Server timeout"),
+    // ));
+    print("timeout");
+  }
+
   void _search() async {
+    _requesting = true;
     if (_textEditingController.text.isEmpty) {
       _streamController.add(null);
+      return;
     }
     _streamController.add("waiting");
-    var response = await get(url + _textEditingController.text,
-        headers: {"Authorization": "Token " + token});
-    print(response.body);
+    Response response = await get(url + _textEditingController.text,
+        headers: {"Authorization": "Token " + token}).catchError((err) {
+      print(err);
+    }).timeout(Duration(seconds: 10), onTimeout: () {
+      _onTimeout(context);
+      return;
+    });
+    if (response == null) {
+      _streamController.add("timeout");
+      return;
+    }
+    if (response.statusCode == 404) {
+      print("fkgf");
+      _streamController.add("No definitions");
+      return;
+    }
+    // print(json.decode(response.body);
+    // if (jsonDecode(response.body)[0]["message"] == "No definition :(") {
+    //   print("no");
+    //   _streamController.add("No Definitions");
+    // }
     _streamController.add(jsonDecode(response.body));
   }
+
+  // void _onTextChanged(String value) {
+  //   Timer(Duration(seconds: 3), () {
+  //     if (!_requesting) {
+  //       print(value);
+  //       _search();
+  //     }
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My DIctionary"),
+        title: Text(
+          "My DIctionary",
+          style:
+              TextStyle(fontFamily: "Montserrat", fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
         bottom: PreferredSize(
             child: Container(
@@ -50,12 +91,24 @@ class _HomePageState extends State<HomePage> {
                       margin: EdgeInsets.all(5),
                       child: Row(
                         children: <Widget>[
+                          SizedBox(
+                            width: 10,
+                          ),
                           Expanded(
                               child: TextField(
+                            // onChanged: _onTextChanged,
+                            style: TextStyle(fontFamily: "Montserrat"),
+                            decoration: InputDecoration.collapsed(
+                                hintText: "Enter text here",
+                                hintStyle: TextStyle(
+                                    fontFamily: "Montserrat",
+                                    color: Colors.black38)),
                             controller: _textEditingController,
                           )),
-                          IconButton(
-                              icon: Icon(Icons.search), onPressed: _search)
+                          Builder(
+                              builder: (context) => IconButton(
+                                  icon: Icon(Icons.search),
+                                  onPressed: () => _search()))
                         ],
                       ))),
             ),
@@ -76,11 +129,19 @@ class _HomePageState extends State<HomePage> {
                   child: CircularProgressIndicator(),
                 );
               }
-              // if (snapshot.data[0]["definitions"] == null ) {
-              //   return Center(
-              //     child: Text("No definitions found"),
-              //   );
-              // }
+              if (snapshot.data == "timeout") {
+                return Center(
+                  child: Text("timeout"),
+                );
+              }
+              if (snapshot.data == "No definitions") {
+                // print("entered");
+                return Center(
+                  child: Text(
+                      "No Definitions found for ${_textEditingController.text}"),
+                );
+              }
+              print(snapshot.data);
               return Container(
                 child: Container(
                   child: ListView.builder(
@@ -91,6 +152,8 @@ class _HomePageState extends State<HomePage> {
                                 margin: EdgeInsets.all(8),
                                 color: Colors.grey[300],
                                 child: ListTile(
+                                  trailing: IconButton(
+                                      icon: Icon(Icons.add), onPressed: () {}),
                                   leading: snapshot.data["definitions"][i]
                                               ["image_url"] ==
                                           null
@@ -100,15 +163,49 @@ class _HomePageState extends State<HomePage> {
                                               snapshot.data["definitions"][i]
                                                   ["image_url"]),
                                         ),
-                                  title: Text(_textEditingController.text +
-                                      " (${snapshot.data["definitions"][i]["type"].toString()})"),
+                                  title: Text(
+                                    "${(i + 1).toString()}. " +
+                                        _textEditingController.text +
+                                        " (${snapshot.data["definitions"][i]["type"].toString()})",
+                                    style: TextStyle(
+                                        fontFamily: "Montserrat",
+                                        fontWeight: FontWeight.bold),
+                                  ),
                                 ),
                               ),
                               Padding(
                                 padding: EdgeInsets.symmetric(horizontal: 10),
                                 child: Text(
-                                    " (${snapshot.data["definitions"][i]["definition"].toString()})"),
-                              )
+                                  "${snapshot.data["definitions"][i]["definition"].toString()}",
+                                  style: TextStyle(
+                                      fontFamily: "Montserrat", fontSize: 15),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 10.0),
+                                child: RichText(
+                                    text: TextSpan(children: [
+                                  TextSpan(
+                                    text: "Example:  ",
+                                    style: TextStyle(
+                                        fontFamily: "Montserrat",
+                                        fontSize: 15,
+                                        color: Colors.blue),
+                                  ),
+                                  TextSpan(
+                                    text:
+                                        "${snapshot.data["definitions"][i]["example"].toString()}",
+                                    style: TextStyle(
+                                        fontFamily: "Montserrat",
+                                        fontSize: 15,
+                                        color: Colors.black),
+                                  ),
+                                ])),
+                              ),
                             ],
                           )),
                 ),
